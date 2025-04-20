faaf42b11daf457c5ee62a6e234f453c068711dd
Mu'adz Abdulloh <150493418+MadZMana@users.noreply.github.com>
2025-04-20 10:55:28 +0700

    Menambahkan comment

--- LAB05.py
+++ LAB05.py
@@ -1,8 +1,10 @@
 import math
 
+# Fungsi hitung volume balok
 def volume_balok(panjang, lebar, tinggi):
     return panjang * lebar * tinggi
 
+# Fungsi hitung volume kerucut
 def volume_kerucut(jari_jari, tinggi):
     return (1/3) * math.pi * (jari_jari**2) * tinggi
 
@@ -12,6 +14,8 @@ volume_balok_total = 0
 volume_kerucut_total = 0
 print(f"\nSelamat datang di Depot Minuman {nama}!")
 print("=========================================")
+
+# Loop input, untuk keluar dari input dengan mengetik STOP
 while True:
     bentuk_galon = input("\nMasukkan bentuk galon yang diinginkan (STOP untuk berhenti): ")
     if bentuk_galon == "BALOK":
@@ -23,6 +27,7 @@ while True:
             continue
         volume = volume_balok(panjang, lebar, tinggi)
         volume_balok_total += volume
+
     if bentuk_galon == "KERUCUT":
         jari_jari = float(input("Masukkan jari-jari kerucut: "))
         tinggi = float(input("Masukkan tinggi kerucut: "))
@@ -31,14 +36,18 @@ while True:
             continue
         volume = volume_kerucut(jari_jari, tinggi)
         volume_kerucut_total += volume
+
     if bentuk_galon == "STOP":
         break
-    elif bentuk_galon != "BALOK" and bentuk_galon != "KERUCUT" and bentuk_galon != "STOP":
-        print("Input tidak benar, masukkan kembali\n")
+
+    if bentuk_galon != "BALOK" and bentuk_galon != "KERUCUT" and bentuk_galon != "STOP":
+        print("Input tidak benar, masukkan kembali")
         continue
 
 total_volume = volume_balok_total + volume_kerucut_total
 total_bayar = total_volume * 700
+
+# Menampilkan hasil akhir
 if total_volume > 0 and total_bayar > 0:
     print("\n=============================================")
     print(f"Total volume air yang dikeluarkan: {total_volume:.2f}")
